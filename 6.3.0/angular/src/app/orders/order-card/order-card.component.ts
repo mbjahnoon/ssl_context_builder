320db75822d780d992e1e495ea0d93141aa6b178
oriyaf1 <oriyaf1@gmail.com>
2021-06-23 19:56:21 +0300

    take status card info design from shared dir.

--- 6.3.0/angular/src/app/orders/order-card/order-card.component.ts
+++ 6.3.0/angular/src/app/orders/order-card/order-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input,} from '@angular/core';
+import {STATUSINFO} from '../../../shared/order/statusCardInfo';
 
 
 
@@ -12,58 +13,8 @@ export class OrderCardComponent implements OnInit {
 
   // possible map for the card colors, 
   //The present will depend on the status of the order.
-  statusInfo = {
-    beforPayment: {
-      bgColor: 'bg-secondary',
-      faIcon: 'fa-comment-dollar',
-      info1: '',
-      info2:'',
-    },
-    preparation: {
-      bgColor: 'bg-info',
-      faIcon: 'fa-spinner fa-spin',
-      info1: '',
-      info2:'',
-    },
-    ready: {
-      bgColor: 'bg-danger',
-      faIcon: 'fa-exclamation-circle',
-      info1: '',
-      info2:'',
-    },
-    delivering: {
-      bgColor: 'bg-warning',
-      faIcon: 'fa-shoe-prints',
-      info1: '',
-      info2:'',
-    },
-    finish: {
-      bgColor: 'bg-success',
-      faIcon: 'fa-check-circle',
-      info1: '',
-      info2:'',
-    },
-  };
+  statusInfo = STATUSINFO;
 
-  // orderData: any = {
-  //   name: 'matan',
-  //   status: 'delivering',
-  //   items: [
-  //     {
-  //       name: 'ציפס 🍟',
-  //       price: 12.9,
-  //     },
-  //     {
-  //       name: 'קולה 🥤',
-  //       price: 8.5,
-
-  //     },
-  //     {
-  //       name: 'גחנון 🍞',
-  //       price: 24,
-  //     },
-  //   ]
-  // }
   @Input() orderData: any;
   constructor() { }
 
