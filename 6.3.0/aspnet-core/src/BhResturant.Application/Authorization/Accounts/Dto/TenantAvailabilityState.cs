5b0613f09b38a144ac54d5d89b448a90a2eef4bc
mbjahnoon <33395838+mbjahnoon@users.noreply.github.com>
2022-02-25 00:55:32 +0200

    clean

--- 6.3.0/aspnet-core/src/BhResturant.Application/Authorization/Accounts/Dto/TenantAvailabilityState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BhResturant.Authorization.Accounts.Dto
-{
-    public enum TenantAvailabilityState
-    {
-        Available = 1,
-        InActive,
-        NotFound
-    }
-}
--- 6.3.0/aspnet-core/src/BhResturant.Core/Configuration/AppSettingNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BhResturant.Configuration
-{
-    public static class AppSettingNames
-    {
-        public const string UiTheme = "App.UiTheme";
-    }
-}
