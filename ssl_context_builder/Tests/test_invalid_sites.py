f43422ed335751c3d2f3a097915d51e3f2fcc50b
mbajhnoon <mbajhnoon@gmail.com>
2022-03-02 02:12:46 +0200

    Improving tests

--- ssl_context_builder/Tests/test_invalid_sites.py
+++ ssl_context_builder/Tests/test_invalid_sites.py
@@ -2,170 +2,162 @@
 This tests suite tests RequestsSecureSession and SslContextBuilder against vulnerable sites in badssl.com
 """
 import pytest
+import requests
+from requests.exceptions import SSLError, ConnectionError
 
 from ssl_context_builder.builder.builder import SslContextBuilder
 from ssl_context_builder.http_impl.requests_wrapper.secure_session import RequestsSecureSession
 
 
-def test_invalid_endpoint_expired_badssl_com():
+def secure_session_func(url):
     ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://expired.badssl.com')
+    with RequestsSecureSession(ctx) as s:
+        return s.session.get(url)
+
+
+def requests_original_func(url):
+    return requests.get(url)
+
+
+invoke_func = secure_session_func
+
+
+def test_invalid_endpoint_expired_badssl_com():
+    url = 'https://expired.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_incomplete_chain_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://incomplete-chain.badssl.com')
+    url = 'https://incomplete-chain.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_invalid_expected_sct_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://invalid-expected-sct.badssl.com')
+    url = 'https://invalid-expected-sct.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_known_interception_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://known-interception.badssl.com')
+    url = 'https://known-interception.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_no_common_name_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://no-common-name.badssl.com')
+    url = 'https://no-common-name.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_no_subject_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://no-subject.badssl.com')
+    url = 'https://no-subject.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_reversed_chain_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://reversed-chain.badssl.com')
+    url = 'https://reversed-chain.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_self_signed_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://self-signed.badssl.com')
+    url = 'https://self-signed.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_untrusted_root_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://untrusted-root.badssl.com')
+    url = 'https://untrusted-root.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_wrong_host_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://wrong.host.badssl.com')
+    url = 'https://wrong.host.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_dh_composite_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://dh-composite.badssl.com')
+    url = 'https://dh-composite.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_dh1024_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://dh1024.badssl.com')
+    url = 'https://dh1024.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_dh480_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://dh480.badssl.com')
+    url = 'https://dh480.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_dh512_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://dh512.badssl.com')
+    url = 'https://dh512.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_3des_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://3des.badssl.com')
+    url = 'https://3des.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_null_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://null.badssl.com')
+    url = 'https://null.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_rc4_md5_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://rc4-md5.badssl.com')
+    url = 'https://rc4-md5.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_rc4_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://rc4.badssl.com')
+    url = 'https://rc4.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_ssl_v2_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://ssl-v2.badssl.com')
+    url = 'https://ssl-v2.badssl.com'
+    with pytest.raises(ConnectionError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_ssl_v3_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://ssl-v3.badssl.com')
+    url = 'https://ssl-v3.badssl.com'
+    with pytest.raises(ConnectionError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_tls_v1_0_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://tls-v1-0.badssl.com')
+    url = 'https://tls-v1-0.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_tls_v1_1_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://tls-v1-1.badssl.com')
+    url = 'https://tls-v1-1.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 
 def test_invalid_endpoint_static_rsa_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with pytest.raises(Exception):
-        with RequestsSecureSession(ctx) as s:
-            s.session.get('https://static-rsa.badssl.com')
+    url = 'https://static-rsa.badssl.com'
+    with pytest.raises(SSLError):
+        invoke_func(url)
 
 # ===================================================================================
 # HPKP, SCT and CRL requires communication to 3rd party servers and are not supported
@@ -181,7 +173,7 @@ def test_invalid_endpoint_static_rsa_badssl_com():
 #         .use_windows_os_cert('CA') \
 #         .use_windows_os_cert('MY') \
 #         .build()
-#     with pytest.raises(Exception):
+#     with pytest.raises(SSLError):
 #         with RequestsSecureSession(ctx) as s:
 #             s.session.get('https://no-sct.badssl.com')
 
@@ -196,7 +188,7 @@ def test_invalid_endpoint_static_rsa_badssl_com():
 #         .use_windows_os_cert('CA') \
 #         .use_windows_os_cert('MY') \
 #         .build()
-#     with pytest.raises(Exception):
+#     with pytest.raises(SSLError):
 #         with RequestsSecureSession(ctx) as s:
 #             s.session.get('https://pinning-test.badssl.com')
 #
@@ -211,6 +203,6 @@ def test_invalid_endpoint_static_rsa_badssl_com():
 #         .use_windows_os_cert('CA') \
 #         .use_windows_os_cert('MY') \
 #         .build()
-#     with pytest.raises(Exception):
+#     with pytest.raises(SSLError):
 #         with RequestsSecureSession(ctx) as s:
 #             s.session.get('https://revoked.badssl.com')
--- ssl_context_builder/Tests/test_valid_sites.py
+++ ssl_context_builder/Tests/test_valid_sites.py
@@ -1,71 +1,64 @@
 """
 This tests suite tests RequestsSecureSession and SslContextBuilder against valid sites in badssl.com
 """
+import requests
 
 from ssl_context_builder.builder.builder import SslContextBuilder
 from ssl_context_builder.http_impl.requests_wrapper.secure_session import RequestsSecureSession
 
-def test_invalid_endpoint_ecc256_badssl_com():
+
+def secure_session_func(url):
     ctx = SslContextBuilder().use_tls_1_2_and_above().build()
     with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://ecc256.badssl.com').status_code == 200
+        return s.session.get(url).status_code
+
+
+def requests_original_func(url):
+    return requests.get(url).status_code
+
+
+invoke_func = secure_session_func
+
+
+def test_invalid_endpoint_ecc256_badssl_com():
+    assert invoke_func('https://ecc256.badssl.com') == 200
 
 
 def test_invalid_endpoint_ecc384_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://ecc384.badssl.com').status_code == 200
+    assert invoke_func('https://ecc384.badssl.com') == 200
 
 
 def test_invalid_endpoint_extended_validation_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://extended-validation.badssl.com').status_code == 200
+    assert invoke_func('https://extended-validation.badssl.com') == 200
 
 
 def test_invalid_endpoint_rsa2048_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://rsa2048.badssl.com').status_code == 200
+    assert invoke_func('https://rsa2048.badssl.com') == 200
 
 
 def test_invalid_endpoint_rsa4096_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://rsa4096.badssl.com').status_code == 200
+    assert invoke_func('https://rsa4096.badssl.com') == 200
 
 
 def test_invalid_endpoint_rsa8192_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://rsa8192.badssl.com').status_code == 200
+    assert invoke_func('https://rsa8192.badssl.com') == 200
 
 
 def test_invalid_endpoint_sha256_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://sha256.badssl.com').status_code == 200
+    assert invoke_func('https://sha256.badssl.com') == 200
 
 
 def test_invalid_endpoint_sha384_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://sha384.badssl.com').status_code == 200
+    assert invoke_func('https://sha384.badssl.com') == 200
 
 
 def test_invalid_endpoint_sha512_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://sha512.badssl.com').status_code == 200
+    assert invoke_func('https://sha512.badssl.com') == 200
 
 
 def test_invalid_endpoint_tls_v1_2_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://tls-v1-2.badssl.com').status_code == 200
+    assert invoke_func('https://tls-v1-2.badssl.com') == 200
 
 
 def test_invalid_endpoint_cbc_badssl_com():
-    ctx = SslContextBuilder().use_tls_1_2_and_above().build()
-    with RequestsSecureSession(ctx) as s:
-        assert s.session.get('https://cbc.badssl.com').status_code == 200
+    assert invoke_func('https://cbc.badssl.com') == 200
